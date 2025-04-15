20ca630b9fbb042481b63c62f20d2722a12954ba
Hanre Delport <hanredelport@gmail.com>
2025-04-15 19:51:36 +0200

    Async try again

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', saltRounds, (err, hash) => {
-    console.log[hash];
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log[res]; //true
+      console.log(res); //true
     });
   });
 
