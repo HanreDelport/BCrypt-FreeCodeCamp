8729bd34cc8f59a3a9ac8bd367dadc79b7774bcb
Hanre Delport <hanredelport@gmail.com>
2025-04-15 19:29:39 +0200

    Hash and compare password

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-
 fccTesting(app);
-const saltRounds = 12;
+const saltRounds = 13;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash('passw0rd!', saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare('passw0rd!', hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 
 //END_ASYNC
