a63357915fda0c14136ec6e8d3f0f4a10d77a429
Hanre Delport <hanredelport@gmail.com>
2025-04-16 08:34:34 +0200

    Trying to get Async right

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ fccTesting(app);
 const saltRounds = 13;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+//Require bcrypt
 const bcrypt = require('bcrypt');
+const hash = "";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
-      console.log(res); //true
-    });
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); //true
+      });
   });
-
-
+  
 //END_ASYNC
+
+
 
 //START_SYNC
 
